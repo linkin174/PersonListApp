f6ac3c2fdfa84e8e8bdea9b2015ea22b86c1debb
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-22 21:11:55 +0500

    progress

--- PersonListApp/Services/DataManager.swift
+++ PersonListApp/Services/DataManager.swift
@@ -6,14 +6,14 @@
 //
 
 class DataManager {
-    static let names = ["Alex", "Tim", "Phil", "Alison", "Robert", "Amanda"]
-    static let surnames = ["Smith", "Cook", "Janson", "Peterson", "Pennyworth", "Blake"]
-    static let emails = ["peterson@apple.com", "iamtheboss@google.com", "donthurtme@yandex.ru", "somename@yahoo.com"]
-    static let phones = ["89823334455", "89823334675", "89823334355", "82223334455", "89323334455", "89553334455"]
+    static let names = ["Alex", "Tim", "Phil", "Alison", "Robert", "Amanda", "Andrew", "Samantha", "Stephany", "Rebecca"]
+    static let surnames = ["Smith", "Cook", "Janson", "Peterson", "Pennyworth", "Blake", "Williams", "Gibson", "Martin", "Jordan"]
+    static let emails = generateEmails(10, nameLenght: 8)
+    static let phones = generatePhones()
     
-    static func createPersons() -> [Person] {
+    static func createPersons(_ quantity: Int) -> [Person] {
         var persons: [Person] = []
-        for _ in 1...10 {
+        for _ in 1...quantity {
             persons.append(Person(firstName: names.randomElement()!,
                                   lastName: surnames.randomElement()!,
                                   email: emails.randomElement()!,
@@ -21,6 +21,30 @@ class DataManager {
         }
         return persons
     }
+    
+    class func generatePhones() -> [String] {
+        var phones: [String] = []
+        for _ in 1...10 {
+            let randomNumber = Int.random(in: 500000000...599999999)
+            phones.append(String(randomNumber))
+        }
+        return phones
+    }
+    
+    class func generateEmails(_ quantity: Int, nameLenght: Int) -> [String] {
+        var emails: [String] = []
+        for _ in 1...quantity {
+            let providers = ["gmail.com", "hotmail.com", "icloud.com", "live.com", "yahoo.com", "livemail.com"]
+            let name = randomString(length: nameLenght)
+            let email = "\(name)@\(providers.randomElement() ?? "email@gmai.com")"
+            emails.append(email)
+        }
+        return emails
+    }
+    
+    class func randomString(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
+    }
 }
-
 
