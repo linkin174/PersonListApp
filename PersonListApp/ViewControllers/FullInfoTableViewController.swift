11f0fd974fb0e236e560d4c5820331cd3d49847e
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-22 23:54:08 +0500

    Need to clean up

--- PersonListApp/ViewControllers/FullInfoTableViewController.swift
+++ PersonListApp/ViewControllers/FullInfoTableViewController.swift
@@ -30,14 +30,14 @@ class FullInfoTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "emailCell", for: indexPath)
-        debugPrint(indexPath)
         let contact = persons[indexPath.section]
         var content = cell.defaultContentConfiguration()
         if indexPath.row == 0 {
             content.text = contact.phoneNumber
-//            content.image = 
+            content.image = UIImage(systemName: "phone")
         } else {
             content.text = contact.email
+            content.image = UIImage(systemName: "mail")
         }
         cell.contentConfiguration = content
         return cell
